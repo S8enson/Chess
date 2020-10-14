8abc0c01f09ad20189d2dc42feb4486f3bb5a2c0
Sam <Sam@localhost>
2020-10-14 21:53:23 +1300

    moved main functions of game to model class

--- src/mvc/Controller.java
+++ src/mvc/Controller.java
@@ -7,6 +7,7 @@ package mvc;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JToggleButton;
 
 /**
  *
@@ -16,23 +17,29 @@ public class Controller implements ActionListener {
 
     public View view;
     public Model model;
+    public boolean first;
+    public JToggleButton firstButton;
+
     /**
-     * Step 4:
-     * Assign view and model to attributes in the constructor, and add ActionListener(this) to the instance of View.
+     * Step 4: Assign view and model to attributes in the constructor, and add
+     * ActionListener(this) to the instance of View.
+     *
      * @param view
-     * @param model 
+     * @param model
      */
     public Controller(View view, Model model) {
         this.view = view;
         this.model = model;
         this.view.addActionListener(this); // Add Actionlistener (the instance of this class) to View.
+        first = true;
     }
 
     /**
-     * Step 5:
-     * Define ActionEvents based on the text displayed on the each button.
-     * Note: You must change the property of relating components to public for external access.
-     * @param e 
+     * Step 5: Define ActionEvents based on the text displayed on the each
+     * button. Note: You must change the property of relating components to
+     * public for external access.
+     *
+     * @param e
      */
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -41,13 +48,34 @@ public class Controller implements ActionListener {
             String username = this.view.unInput.getText(); // Obtain username.
             String password = this.view.pwInput.getText(); // Obtain password.
             this.model.checkName(username, password); // Pass above variables to model. Go to the checkName() of Model.java for step 6.
-        } else if (command.equals("Next")) { // Next button
+        } else if (command.equals("New Game")) { // Next button
+            // Step 8:
+            // Go to the checkAnswer() and quitGame() of Model.java.
+            this.view.login(); // Check user's answer.
+        } else if (command.equals("Leaderboard")) { // Next button
             // Step 8:
             // Go to the checkAnswer() and quitGame() of Model.java.
             //this.model.checkAnswer(this.view.calcSolution.getText()); // Check user's answer.
         } else if (command.equals("Quit")) { // Quit button
-            this.model.quitGame(); // Record user's current score.
+            //this.model.quitGame(); // Record user's current score.
+        } else if (command.equals("test")) { // Quit button
+            //this.model.newGame();
+            this.view.test();
+            this.model.board.drawBoard(this.view.boardPanel.getGraphics());
+            // Record user's current score.
+        } else if (e.getSource() instanceof JToggleButton) {
+            if (first) {
+                String start = ((JToggleButton) e.getSource()).getText();
+                first = false;
+                firstButton = ((JToggleButton) e.getSource());
+            } else if (!first) {
+                String end = ((JToggleButton) e.getSource()).getText();
+                first = true;
+                ((JToggleButton) e.getSource()).setSelected(false);
+                firstButton.setSelected(false);
+            }
         }
+
     }
 
 }
